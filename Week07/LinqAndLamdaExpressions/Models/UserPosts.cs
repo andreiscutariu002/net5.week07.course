bc172e975e43b36f93024dfcf9e4c92effcdd6d8
constantin-puscasu <constantin.puscasu@webbeds.com>
2019-11-30 14:00:06 +0200

    Implemented LINQ queries

---
+++ Week07/LinqAndLamdaExpressions/Models/UserPosts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAndLamdaExpressions.Models
+{
+    public class UserPosts
+    {
+        private User user;
+        private List<Post> posts;
+
+
+        public UserPosts()
+        {
+            this.Posts = new List<Post>();
+        }
+
+        public User User { get => user; set => user = value; }
+        public List<Post> Posts { get => posts; set => posts = value; }
+    }
+}
